d67788e6d9ac9c7b272ac5fa6f8bf8b7e2534993
Yerson Garcia <Yersoncampo79@gmail.com>
2025-07-25 20:14:31 -0500

    prueba solucion base de datos desarrollo

--- Configuration/PostgresSettings.cs
+++ Configuration/PostgresSettings.cs
@@ -2,11 +2,11 @@
 {
     public class PostgresSettings
     {
-        public string Database { get; set; } = default!;
-        public string Username { get; set; } = default!;
-        public string Password { get; set; } = default!;
-        public string Host { get; set; } = default!;
-        public int Port { get; set; }
+        public required string Host { get; set; }
+        public required string Port { get; set; }
+        public required string Database { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
 
--- ProyectosGradoApi/Program.cs
+++ ProyectosGradoApi/Program.cs
@@ -18,24 +18,42 @@ builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-// Configuración desde User Secrets
-var postgresSettings = builder.Configuration
+if (builder.Environment.IsDevelopment())
+{
+    // Configuración desde User Secrets
+    var postgresSettings = builder.Configuration
     .GetSection("secreto")
     .Get<PostgresSettings>();
 
-var connectionString = $"Host={postgresSettings?.Host};Port={postgresSettings?.Port};" +
-                       $"Database={postgresSettings?.Database};Username={postgresSettings?.Username};" +
-                       $"Password={postgresSettings?.Password}";
+    var connectionString = $"Host={postgresSettings?.Host};Port={postgresSettings?.Port};" +
+                           $"Database={postgresSettings?.Database};Username={postgresSettings?.Username};" +
+                           $"Password={postgresSettings?.Password}";
 
-// Configurar DbContext
+    // Configurar DbContext
+    builder.Services.AddDbContext<RepositoryDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    // ❗ Mover esta línea antes de Build
+    builder.Services.Configure<PostgresSettings>(
+        builder.Configuration.GetSection("secreto"));
+}
+else { 
+var postgresSettings = new PostgresSettings
+{
+    Host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "",
+    Port = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "11550",
+    Database = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "",
+    Username = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "",
+    Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? ""
+};
+
+var connectionString = $"Host={postgresSettings.Host};Port={postgresSettings.Port};" +
+                       $"Database={postgresSettings.Database};Username={postgresSettings.Username};" +
+                       $"Password={postgresSettings.Password}";
+
 builder.Services.AddDbContext<RepositoryDbContext>(options =>
     options.UseNpgsql(connectionString));
-
-// ❗ Mover esta línea antes de Build
-builder.Services.Configure<PostgresSettings>(
-    builder.Configuration.GetSection("secreto"));
-
+}
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -47,10 +65,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapGet("/index.html", () => Results.Redirect("/swagger"));
-    app.UseCors("AllowAnyOrigin");
+
 }
 
 if (app.Environment!.IsDevelopment())
